b07b306068e741a547b3b90c584eb2e49c83ffef
Darshan Shankar <darshanshankar@machine.local>
2014-09-13 16:09:20 -0700

    sample objc code

---
+++ sample.m
@@ -0,0 +1,35 @@
+//
+//  File.m
+//  Binary
+//
+//  Created by Darshan Shankar on 9/5/14.
+//  Copyright (c) 2014 Darshan Shankar. All rights reserved.
+//
+
+#import "File.h"
+
+@implementation File
+@synthesize filename;
+@synthesize extension;
+@synthesize language;
+
+static File *instance = nil;
+
++ (File *)getInstance
+{
+    @synchronized(self){
+        if(instance == nil){
+            instance = [File new];
+            instance.filename = @"/sample";
+            instance.extension = @"css";
+            instance.language = @"css";
+            instance.theme = @"okaida";
+            instance.tabIndent = YES;
+            instance.tabSpaces = 4;
+            
+        }
+    }
+    return instance;
+}
+
+@end
